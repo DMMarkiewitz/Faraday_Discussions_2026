9966219c01419d0d4d692f8dfe3ca23bfd5ec18e
zg1413 <118224463+zg1413@users.noreply.github.com>
2026-05-21 11:18:19 +0100

    Bulk activity of species

--- Active/Runners/Activity_Bulk.m
+++ Active/Runners/Activity_Bulk.m
@@ -1,84 +1,125 @@
-%Merging Activities
-close all
-clear all
+clearvars
+clc
 
-%Physical Constants
-T = 300; 
-Na = 6.022e23; 
-kB = 1.38e-23;  
-e = 1.6e-19;
-beta = 1/(kB*T);
+%% System properties and bulk quantities
+fa  = 3;          % Anion functionality
+fc  = 4;          % Cation functionality
+xic = 0.4;        % Cation volume ratio
+xia = 10.8;       % Anion volume ratio
+MMw = 18.02/1000;  % Water molar mass [kg/mol]
+L   = 0.2527;     % Association constant at 21 m
 
-%General Systems Properties and Bulk Quantities
-fa = 3;
-fc = 4;
-xic = 0.4;
-xia = 10.8;
-MMw=18.02/1000; %kg/mol molar mass of water
-L = 0.2527;
+% Composition grid
+mol_f_ref = 1 / ((1 / MMw / 0.5) + 1);
+mol_f = [mol_f_ref:0.001:0.02-0.001, 0.02:0.0025:0.35];
+molality = (1 / MMw) .* (1 ./ (1 ./ mol_f - 1));
 
-mol_f_ref = 1/((1/MMw/0.5)+1);
-mol_f = [mol_f_ref:0.001:0.02-0.001,0.02:0.0025:.35];
-molality = 1/MMw.*(1./(1./mol_f-1));
-for i = 1:length(mol_f)
-    phi_p_b(i) = xic/(1/mol_f(i)-1+xic+xia);
-    phi_m_b(i) = xia/xic*phi_p_b(i);
-    phi_0_b(i) = 1 - phi_p_b(i) - phi_m_b(i);
-        
-    pc(i) = f_pc(phi_0_b(i),fc*phi_p_b(i)/xic,fa*phi_m_b(i)/xia,L);
-    pa(i) = f_pa(phi_0_b(i),fc*phi_p_b(i)/xic,fa*phi_m_b(i)/xia,L);
-    pc0(i) = f_pc0(phi_0_b(i),fc*phi_p_b(i)/xic,fa*phi_m_b(i)/xia,L);
-    p0(i) = f_p0(phi_0_b(i),fc*phi_p_b(i)/xic,fa*phi_m_b(i)/xia,L);
+nPts = numel(mol_f);
+phi_p_b = zeros(nPts, 1);
+phi_m_b = zeros(nPts, 1);
+phi_0_b = zeros(nPts, 1);
+pc   = zeros(nPts, 1);
+pa   = zeros(nPts, 1);
+pc0  = zeros(nPts, 1);
+p0   = zeros(nPts, 1);
 
+%% Bulk composition and association probabilities
+for k = 1:nPts
+    [phi_p_b(k), phi_m_b(k), phi_0_b(k)] = bulkFractions(mol_f(k), xic, xia);
+
+    psi_p = fc * phi_p_b(k) / xic;
+    psi_m = fa * phi_m_b(k) / xia;
+
+    pc(k)  = f_pc(phi_0_b(k), psi_p, psi_m, L);
+    pa(k)  = f_pa(phi_0_b(k), psi_p, psi_m, L);
+    pc0(k) = f_pc0(phi_0_b(k), psi_p, psi_m, L);
+    p0(k)  = f_p0(phi_0_b(k), psi_p, psi_m, L);
 end
 
-mu_p = 1+fc+log((1+fc/xic)*phi_p_b.*(pc0).^fc);
-mu_m = 1+log(phi_m_b.*(1-pa).^fa);
-mu_0 = 1+log(phi_0_b.*(1-p0));
+%% Activity predictions
+mu_p = 1 + fc + log((1 + fc / xic) .* phi_p_b .* (pc0 .^ fc));
+mu_m = 1 + log(phi_m_b .* (1 - pa) .^ fa);
+mu_0 = 1 + log(phi_0_b .* (1 - p0));
 
-% Activity Predictions
-figure()
-plot(molality,mu_p-mu_p(1)-(4*mu_0-4*mu_0(1))*1,'r-','linewidth',1.5)
-hold on
-plot(molality,mu_p-mu_p(1)-(4*mu_0-4*mu_0(1))*0,'r--','linewidth',1.5)
-plot(molality,mu_m-mu_m(1),'b:','linewidth',1.5)
-plot(molality,mu_0-mu_0(1),'k-.','linewidth',1.5)
-xlim([0,21])
-ylim([-5.5,16])
-box on
-leg=legend({'Li$^+$','Hydrated Li$^+$','TFSI$^-$','H$_2$O'},'location','north','interpreter','latex','Orientation','horizontal','box','off');
-leg.ItemTokenSize = [18,18];
-pbaspect([(1+sqrt(5))/2,1,1])
-set(groot, 'defaultAxesTickLabelInterpreter','latex'); 
+% Reference-state shift used in the original script
+mu_p_hydrated = mu_p - mu_p(1) - (4 * (mu_0 - mu_0(1)));
+mu_p_unhydrated = mu_p - mu_p(1);
+mu_m_rel = mu_m - mu_m(1);
+mu_0_rel = mu_0 - mu_0(1);
+
+%% Plot
+fig = figure('Color', 'w');
+ax = axes(fig);
+hold(ax, 'on');
+
+plot(ax, molality, mu_p_hydrated, 'r-',  'LineWidth', 1.5);
+plot(ax, molality, mu_p_unhydrated, 'r--', 'LineWidth', 1.5);
+plot(ax, molality, mu_m_rel, 'b:', 'LineWidth', 1.5);
+plot(ax, molality, mu_0_rel, 'k-.', 'LineWidth', 1.5);
+
+xlim(ax, [0, 21]);
+ylim(ax, [-5.5, 16]);
+box(ax, 'on');
+
+leg = legend(ax, {'Li$^+$', 'Hydrated Li$^+$', 'TFSI$^-$', 'H$_2$O'}, ...
+    'Location', 'north', 'Interpreter', 'latex', 'Orientation', 'horizontal', 'Box', 'off');
+leg.ItemTokenSize = [18, 18];
+
+pbaspect(ax, [(1 + sqrt(5)) / 2, 1, 1]);
+
+set(groot, 'defaultAxesTickLabelInterpreter', 'latex');
 set(groot, 'DefaultTextInterpreter', 'latex');
-xlabel(['LiTFSI molality, m'],'interpreter','latex', 'fontsize', 16)
-ylabel(['Ln($a_i$/$a_i^\theta$)'],'interpreter','latex', 'fontsize', 16)
-g=gcf;
-g.Renderer='painters';
-set(gca, 'Xcolor', 'k');
-set(gca, 'Ycolor', 'k');
-set(gca, 'FontSize', 14);
-set(gca, 'LineWidth', 1.5);
-set(gca, 'Layer', 'Top');
-set(gca, 'color', 'none');
-set(g,'InvertHardcopy','on');
-exportgraphics(gcf,['Dual_Sticky_WiSE_Bulk 21m_L_05m_ref.eps'],'Resolution',600)
-exportgraphics(gcf,['Dual_Sticky_WiSE_Bulk 21m_L_05m_ref.jpeg'],'Resolution',600)
-saveas(gcf,['Dual_Sticky_WiSE_Bulk 21m_L_05m_ref.fig'])
-    
-function pc = f_pc(phi_0,psi_p,psi_m,L)
-    pc = (phi_0 - psi_p + L*(psi_p + psi_m) - sqrt(4*phi_0*psi_p*(L-1) + (L*(psi_m - psi_p) + psi_p + phi_0)^2))/(2*(L-1)*psi_p);
+
+xlabel(ax, 'LiTFSI molality, m', 'Interpreter', 'latex', 'FontSize', 16);
+ylabel(ax, 'Ln($a_i$/$a_i^\theta$)', 'Interpreter', 'latex', 'FontSize', 16);
+
+set(ax, ...
+    'XColor', 'k', ...
+    'YColor', 'k', ...
+    'FontSize', 14, ...
+    'LineWidth', 1.5, ...
+    'Layer', 'top', ...
+    'Color', 'none');
+
+set(fig, 'Renderer', 'painters', 'InvertHardcopy', 'on');
+
+% Export outputs
+baseName = 'Dual_Sticky_WiSE_Bulk_21m_L_05m_ref';
+exportgraphics(fig, [baseName '.eps'], 'Resolution', 600);
+exportgraphics(fig, [baseName '.jpeg'], 'Resolution', 600);
+savefig(fig, [baseName '.fig']);
+
+%% Local functions
+function [phi_p_b, phi_m_b, phi_0_b] = bulkFractions(mol_f, xic, xia)
+    phi_p_b = xic / (1 / mol_f - 1 + xic + xia);
+    phi_m_b = (xia / xic) * phi_p_b;
+    phi_0_b = 1 - phi_p_b - phi_m_b;
 end
 
-function pa = f_pa(phi_0,psi_p,psi_m,L)
-    pa = (phi_0 - psi_p + L*(psi_p + psi_m) - sqrt(4*phi_0*psi_p*(L-1) + (L*(psi_m - psi_p) + psi_p + phi_0)^2))/(2*(L-1)*psi_m);
+function pc = f_pc(phi_0, psi_p, psi_m, L)
+    discriminant = 4 * phi_0 * psi_p * (L - 1) + ...
+        (L * (psi_m - psi_p) + psi_p + phi_0)^2;
+    pc = (phi_0 - psi_p + L * (psi_p + psi_m) - sqrt(discriminant)) / ...
+        (2 * (L - 1) * psi_p);
 end
 
-function pc0 = f_pc0(phi_0,psi_p,psi_m,L)
-    pc0 = (phi_0 + psi_p + L*(-psi_p + psi_m) - sqrt(4*phi_0*psi_p*(L-1) + (L*(psi_m - psi_p) + psi_p + phi_0)^2))/(2*(1-L)*psi_p);
+function pa = f_pa(phi_0, psi_p, psi_m, L)
+    discriminant = 4 * phi_0 * psi_p * (L - 1) + ...
+        (L * (psi_m - psi_p) + psi_p + phi_0)^2;
+    pa = (phi_0 - psi_p + L * (psi_p + psi_m) - sqrt(discriminant)) / ...
+        (2 * (L - 1) * psi_m);
 end
 
-function p0 = f_p0(phi_0,psi_p,psi_m,L)
-    p0 = (phi_0 + psi_p + L*(-psi_p + psi_m) - sqrt(4*phi_0*psi_p*(L-1) + (L*(psi_m - psi_p) + psi_p + phi_0)^2))/(2*(1-L)*phi_0);
+function pc0 = f_pc0(phi_0, psi_p, psi_m, L)
+    discriminant = 4 * phi_0 * psi_p * (L - 1) + ...
+        (L * (psi_m - psi_p) + psi_p + phi_0)^2;
+    pc0 = (phi_0 + psi_p + L * (-psi_p + psi_m) - sqrt(discriminant)) / ...
+        (2 * (1 - L) * psi_p);
 end
 
+function p0 = f_p0(phi_0, psi_p, psi_m, L)
+    discriminant = 4 * phi_0 * psi_p * (L - 1) + ...
+        (L * (psi_m - psi_p) + psi_p + phi_0)^2;
+    p0 = (phi_0 + psi_p + L * (-psi_p + psi_m) - sqrt(discriminant)) / ...
+        (2 * (1 - L) * phi_0);
+end
